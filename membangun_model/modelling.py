46a176c14d4cc2eea2898e4417024240f9535a69
XnomaX <nurarief1123@gmail.com>
2025-06-14 05:51:19 +0700

    update

---
+++ membangun_model/modelling.py
@@ -0,0 +1,36 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import zipfile 
+
+# Aktifkan auto-logging
+mlflow.set_tracking_uri("file:///D:/MLflowRuns")
+mlflow.sklearn.autolog()
+# Ekstraksi dataset
+zip_path = "../preprocessing/dataset_preprocessing.zip"  
+    
+with  zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall('')
+
+# Load data
+df = pd.read_csv('dataset_preprocessing.csv')
+
+# Pisahkan fitur dan target
+X = df.drop(columns=['survived'])
+y = df['survived']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# MLflow Tracking
+with mlflow.start_run():
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"Akurasi: {acc}")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
