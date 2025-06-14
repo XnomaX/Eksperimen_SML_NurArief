d9c98747c0193f26d96d8cc9e879d82086e6e4c4
XnomaX <nurarief1123@gmail.com>
2025-06-14 11:27:08 +0700

    Update modelling

--- membangun_model/modelling.py
+++ membangun_model/modelling.py
@@ -5,9 +5,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import zipfile 
+import tempfile
+import os
+
+# Set direktori sementara untuk MLflow 
+tempfile.tempdir = "D:/mlflow_temp"
+os.makedirs(tempfile.tempdir, exist_ok=True)
 
 # Aktifkan auto-logging
 mlflow.set_tracking_uri("file:///D:/MLflowRuns")
+mlflow.set_experiment("msml-basic")
 mlflow.sklearn.autolog()
 # Ekstraksi dataset
 zip_path = "../preprocessing/dataset_preprocessing.zip"  
@@ -27,7 +34,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # MLflow Tracking
 with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
+    model = RandomForestClassifier( random_state=42)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
