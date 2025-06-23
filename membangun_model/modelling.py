645aa7defa33d6f09fab9671ac83be7711ab3c50
XnomaX <nurarief1123@gmail.com>
2025-06-24 03:14:28 +0700

    Changes to be committed: 	new file:   membangun_model/DagHubs.txt 	new file:   membangun_model/dataset_preprocessing.zip 	modified:   membangun_model/modelling.py 	new file:   membangun_model/ss_ArtifactBasic_mlfow.png 	modified:   membangun_model/ss_DashboardBasic_Mlflow.png 	modified:   preprocessing/Eksperimen_Nur_Arief_Fauzian_R.ipynb
    Changes not staged for commit:
    	deleted:    DagHubs.txt
    	deleted:    membangun_model/metrics.csv
    	deleted:    membangun_model/mlruns/0/meta.yaml
    	deleted:    membangun_model/mlruns/157961825692968274/meta.yaml
    	deleted:    membangun_model/mlruns/794098743695549855/meta.yaml
    	deleted:    membangun_model/params.yml

--- membangun_model/modelling.py
+++ membangun_model/modelling.py
@@ -6,12 +6,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import zipfile 
 
-# Aktifkan auto-logging
-mlflow.set_experiment("msml-basic")
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.sklearn.autolog()
+
 # Ekstraksi dataset
-zip_path = "../preprocessing/dataset_preprocessing.zip"  
+zip_path = "./dataset_preprocessing.zip"  
     
 with  zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall('')
@@ -22,16 +19,17 @@ df = pd.read_csv('dataset_preprocessing.csv')
 # Pisahkan fitur dan target
 X = df.drop(columns=['survived'])
 y = df['survived']
-
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Aktifkan auto-logging
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("msml-basic")
+mlflow.sklearn.autolog()
 
 # MLflow Tracking
 with mlflow.start_run():
     model = RandomForestClassifier( random_state=42)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Akurasi: {acc}")
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model", input_example=X_test.iloc[:5])
+    accuracy = accuracy_score(y_test, y_pred) 
